3bf1806495b6457564ae23f03e2146139445c967
BrunoOliveiraHHH <bruno.d.l.oliveira@outlook.com>
2020-10-12 00:38:43 -0300

    Inclusão da coluna Durabilidade na tbArma

--- PROCEDURE/Armas/adiciona_arma.sql
+++ PROCEDURE/Armas/adiciona_arma.sql
@@ -1,16 +1,4 @@
-/* Script para criação da tabela o_inicio_do_fim_v2.tb_arma
-
-/----------- Modificações -----------------------------/
-/ Nome           -   Data     - Descrição              /
-/------------------------------------------------------/
-/ Bruno Oliveira - 25/05/2020 - Criação.
-
-*/
-
-USE o_inicio_do_fim_v2;
-GO
-
-CREATE PROCEDURE adiciona_arma
+ALTER PROCEDURE [dbo].[adiciona_arma]
     @nome_arma VARCHAR(255),
     @custo_arma INT,
     @dano_arma VARCHAR(255),
@@ -18,6 +6,7 @@ CREATE PROCEDURE adiciona_arma
     @peso_arma VARCHAR(255),
     @propriedade_arma VARCHAR(255),
     @municao_arma 	INT,
+	@durabilidade_arma INT,
     @observacao_arma VARCHAR(255)
 AS
 
@@ -32,9 +21,8 @@ BEGIN
     IF (@id_arma <> @total)
 	    BEGIN
         INSERT INTO  [dbo].[tb_arma]
-            (id_arma, nome, custo, dano, tipo_de_dano, peso, propriedade, municao, observacao)
+            (id_arma, nome, custo, dano, tipo_de_dano, peso, propriedade, municao, durabilidade, observacao)
         VALUES
-            (@id_arma, @nome_arma, @custo_arma, @dano_arma, @tipo_de_dano_arma, @peso_arma, @propriedade_arma, @municao_arma, @observacao_arma);
+            (@id_arma, @nome_arma, @custo_arma, @dano_arma, @tipo_de_dano_arma, @peso_arma, @propriedade_arma, @municao_arma, @durabilidade_arma, @observacao_arma);
     END;
 END;
-GO
